ac25cc15fd299b84bc28c4838b913e03be3158ff
Luka <luka.pevec@fer.hr>
2017-06-12 17:12:50 +0200

    Layout pub and sub done
    Publishers and subscribers defined

--- morus_control/include/morus_control/mass_ctl_attitude_mpc_node.h
+++ morus_control/include/morus_control/mass_ctl_attitude_mpc_node.h
@@ -1,8 +1,13 @@
 #ifndef PROJECT_MASS_CTL_ATTITUDE_MPC_NODE_H
 #define PROJECT_MASS_CTL_ATTITUDE_MPC_NODE_H
 
+#include <math.h>
+
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
+#include "std_msgs/Float32.h"
+#include "sensor_msgs/Imu.h"
+#include "rosgraph_msgs/Clock.h"
 
 namespace mav_control_attitude {
     class MPCAttitudeControllerNode{
@@ -15,13 +20,40 @@ namespace mav_control_attitude {
         ros::NodeHandle nh_;
         ros::NodeHandle private_nh_;
 
+        double count_;
+        bool start_flag_;
+
         // publishers
         ros::Publisher pub_mass0_;
         ros::Publisher pub_mass1_;
         ros::Publisher pub_mass2_;
         ros::Publisher pub_mass3_;
 
-        float count;
+        // subscribers
+        ros::Subscriber imu_;
+            void ahrs_cb        (const sensor_msgs::Imu& msg);
+            struct euler_mv {
+                double x;
+                double y;
+                double z;
+            } euler_mv_;
+            struct euler_rate_mv {
+                double x;
+                double y;
+                double z;
+            } euler_rate_mv_;
+
+        ros::Subscriber mot_vel_ref_;
+            void mot_vel_ref_cb (const std_msgs::Float32& msg);
+            float w_sp_;
+
+        ros::Subscriber euler_ref_;
+            void euler_ref_cb   (const geometry_msgs::Vector3& msg);
+            geometry_msgs::Vector3 euler_sp_;
+
+        ros::Subscriber clock_;
+            void clock_cb       (const rosgraph_msgs::Clock& msg);
+            rosgraph_msgs::Clock clock_read_;
     };
 }
 #endif //PROJECT_MASS_CTL_ATTITUDE_MPC_NODE_H
--- morus_control/src/nodes/mass_ctl_attitude_mpc_node.cpp
+++ morus_control/src/nodes/mass_ctl_attitude_mpc_node.cpp
@@ -6,34 +6,93 @@ MPCAttitudeControllerNode::MPCAttitudeControllerNode(const ros::NodeHandle& nh,
         : nh_(nh),
           private_nh_(private_nh)
 {
-    // Publishers ( nh -> )
+    count_ = 0.0; // temp variable for the publishers
+    start_flag_ = false;            // flag indicates if the first measurement is received
+
+    // Publishers  ( nh -> )
     pub_mass0_ = nh_.advertise<std_msgs::Float64>("movable_mass_0_position_controller/command", 1);
     pub_mass1_ = nh_.advertise<std_msgs::Float64>("movable_mass_1_position_controller/command", 1);
     pub_mass2_ = nh_.advertise<std_msgs::Float64>("movable_mass_2_position_controller/command", 1);
     pub_mass3_ = nh_.advertise<std_msgs::Float64>("movable_mass_3_position_controller/command", 1);
 
-    count = 0.0;
-
     // Subscribers ( nh <- )
-    //ros::Subscriber sub = nh.subscribe("imu", 1, chatterCallback);
+    imu_ = nh_.subscribe("imu", 1, &MPCAttitudeControllerNode::ahrs_cb, this);
+    mot_vel_ref_ = nh_.subscribe("mot_vel_ref", 1, &MPCAttitudeControllerNode::mot_vel_ref_cb, this);
+    euler_ref_ = nh_.subscribe("euler_ref", 1, &MPCAttitudeControllerNode::euler_ref_cb, this);
+    clock_ = nh_.subscribe("/clock", 1, &MPCAttitudeControllerNode::clock_cb, this);
 }
 
-MPCAttitudeControllerNode::~MPCAttitudeControllerNode()
-{
+MPCAttitudeControllerNode::~MPCAttitudeControllerNode() {
+}
+
+void MPCAttitudeControllerNode::ahrs_cb(const sensor_msgs::Imu &msg) {
+    /**
+    AHRS callback. Used to extract roll, pitch, yaw and their rates.
+            We used the following order of rotation - 1)yaw, 2) pitch, 3) roll
+    :param msg: Type sensor_msgs/Imu
+    */
+    if (!start_flag_){
+        start_flag_ = true;
+    }
+
+    // read the msg
+    double qx = msg.orientation.x;
+    double qy = msg.orientation.y;
+    double qz = msg.orientation.z;
+    double qw = msg.orientation.w;
+
+    // conversion quaternion to euler (yaw - pitch - roll)
+    euler_mv_.x = atan2(2 * (qw * qx + qy * qz), qw * qw - qx * qx - qy * qy + qz * qz);
+    euler_mv_.y =  asin(2 * (qw * qy - qx * qz));
+    euler_mv_.z = atan2(2 * (qw * qz + qx * qy), qw * qw + qx * qx - qy * qy - qz * qz);
+
+    // gyro measurements (p,q,r)
+    double p = msg.angular_velocity.x;
+    double q = msg.angular_velocity.y;
+    double r = msg.angular_velocity.z;
+
+    double sx = sin(euler_mv_.x); // sin(roll)
+    double cx = cos(euler_mv_.x); // cos(roll)
+    double cy = cos(euler_mv_.y); // cos(pitch)
+    double ty = tan(euler_mv_.y); // cos(pitch)
+
+    // conversion gyro measurements to roll_rate, pitch_rate, yaw_rate
+    euler_rate_mv_.x = p + sx * ty * q + cx * ty * r;
+    euler_rate_mv_.y = cx * q - sx * r;
+    euler_rate_mv_.z = sx / cy * q + cx / cy * r;
+}
+
+void MPCAttitudeControllerNode::mot_vel_ref_cb(const std_msgs::Float32 &msg) {
+    /**
+    Referent motor velocity callback. (This should be published by height controller).
+    :param msg: Type Float32
+    */
+    w_sp_ = msg.data;
+
+}
+
+void MPCAttitudeControllerNode::euler_ref_cb(const geometry_msgs::Vector3 &msg) {
+    /**
+    Euler ref values callback.
+    :param msg: Type Vector3 (x-roll, y-pitch, z-yaw)
+    */
+    euler_sp_ = msg;
+}
+
+void MPCAttitudeControllerNode::clock_cb(const rosgraph_msgs::Clock& msg) {
+    clock_read_ = msg;
 }
 
 void MPCAttitudeControllerNode::Publish() {
     // MAIN ALGORITHM THAT RUNS FOREVER !
     std_msgs::Float64 msg;
-    msg.data = count;
+    msg.data = count_;
 
     // publish the right control actions
     pub_mass0_.publish(msg);
     pub_mass1_.publish(msg);
     pub_mass2_.publish(msg);
     pub_mass3_.publish(msg);
-
-    count+= 1.0;
 }
 }
 
@@ -50,9 +109,8 @@ int main(int argc, char **argv)
     while (ros::ok()){
         ros::spinOnce(); // get all msg-s from subscribers
         MPC_attitude_controller.Publish();
-        loop_rate.sleep(); //
+        loop_rate.sleep(); // watch that sampling time is correct
     }
-
 
     return 0;
 }
